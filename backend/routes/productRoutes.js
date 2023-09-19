936d5f72a25abea50cb0b4bf681905df491894bd
Boyu <boyu@gmail.com>
2023-09-18 20:30:31 -0400

    backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,14 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    //Passing an empty object to find all of them
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  })
-);
-
+router.get("/", getProducts);
+// Alternatively
+//router.get('/').get(getProducts);
+router.get("/:id", getProductById);
+// Alternatively
+//router.get('/:id').get(getProduct);
 export default router;
