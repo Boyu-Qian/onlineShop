ba8806c2200dd803b8e039d8a68b170ee1d106eb
Boyu Qian <101378701+Boyu-Qian@users.noreply.github.com>
2023-10-07 19:22:34 -0400

    Update server.js
    C

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const port = process.env.PORT;
 import cors from "cors";
 connectDB();
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 /*
 body parser middleware
